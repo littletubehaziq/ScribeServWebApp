2bb6e79ef8b5b20627c5f2c2212a82c97adcb449
ziQorita <zaheerulhaziq@gmail.com>
2020-10-04 05:22:40 +0800

    Testing of Authenticator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <AmplifySignOut />
+    My App
+  </div>
+);
 
-export default App;
+export default withAuthenticator(App);
