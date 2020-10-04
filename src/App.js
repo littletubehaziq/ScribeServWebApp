7f308a66a536f9a77f4f9a8110c3a3b173674137
Serene Lo <serenelo@users-MacBook-Air.local>
2020-10-04 17:31:28 +0800

    added auth

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Amplify, { API } from 'aws-amplify';
-
-Amplify.configure({
-    // OPTIONAL - if your API requires authentication 
-    Auth: {
-        // REQUIRED - Amazon Cognito Identity Pool ID
-        //identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab',
-        // REQUIRED - Amazon Cognito Region
-        region: 'us-east-1', 
-        // OPTIONAL - Amazon Cognito User Pool ID
-        userPoolId: 'us-east-1_f0zGJ51v2', 
-        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-        userPoolWebClientId: 'a75kluojk9ad1aqkq1gib18ctu0',
-    }
-});
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
